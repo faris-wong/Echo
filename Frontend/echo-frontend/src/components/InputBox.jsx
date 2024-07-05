4972765a1f9d4ef04507cacd00c3816e0e639d72
ashferoz <ash.safrina@gmail.com>
2024-07-05 17:20:09 +0800

    Input box fixed to bottom of page.

--- Frontend/echo-frontend/src/components/InputBox.jsx
+++ Frontend/echo-frontend/src/components/InputBox.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import styles from './css/InputBox.module.css'
+import React from "react";
+import styles from "./css/InputBox.module.css";
 
 const InputBox = () => {
-    return (
-        <div className={styles.container}>
-            <input type="text" placeholder='Enter message'/>
-            <div className={styles.sendBtn}>
-            <i class="fa-solid fa-paper-plane"></i>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.window}>
+    <div className={styles.container}>
+      <input type="text" placeholder="Enter message" />
+      <div className={styles.sendBtn}>
+        <i class="fa-solid fa-paper-plane"></i>
+      </div>
+    </div>
+    </div>
+  );
 };
 
 export default InputBox;
