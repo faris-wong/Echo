2b5e3d49b9ae37fc99360cc7b050afeeb54dea56
vicknesh95 <vickababa95@hotmail.com>
2024-07-05 18:11:57 +0800

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,7 @@ const auth = require("./src/routers/auth");
 const profile = require("./src/routers/profile");
 
 const connectDB = require("./src/database/database");
+const Messages = require("./src/routers/Messages");
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
@@ -27,7 +28,11 @@ app.use(express.urlencoded({ extended: false }));
 
 //put all api router routes here below the top line 20 to 24
 app.use("/auth", auth);
+<<<<<<< Updated upstream
 app.use("/profile", profile);
+=======
+app.use("/messages", Messages);
+>>>>>>> Stashed changes
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
