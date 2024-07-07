cb98f664a0e446079f2599963f6fd1342e6b4ba7
vicknesh95 <vickababa95@hotmail.com>
2024-07-07 16:06:24 +0800

    set up routes for different components

--- Frontend/echo-frontend/src/App.jsx
+++ Frontend/echo-frontend/src/App.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import ChatRoom from "./components/ChatRoom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ProfilePage from "./components/ProfilePage";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   const queryClient = new QueryClient();
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        {/* <ChatRoom /> */}
-        <ProfilePage />
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="Community" element={<ChatRoom />} />
+            <Route path="Profile" element={<ProfilePage />} />
+          </Routes>
+        </div>
       </QueryClientProvider>
     </>
   );
---
+++ Frontend/echo-frontend/src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "./css/Navbar.module.css";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <NavLink to="/" className={styles.title}>
+        ECHO
+      </NavLink>
+      <ul className={styles.navlist}>
+        <li>
+          <NavLink to="/Home">HOME</NavLink>
+        </li>
+        <li>
+          <div className={styles.communitiesList}> COMMUNITIES </div>
+
+          <NavLink to="/Community">Indie</NavLink>
+        </li>
+        <li className={styles.profile}>
+          <NavLink to="/Profile">
+            <div class={styles.profileContainer}>
+              <img
+                src="https://img.freepik.com/free-photo/androgynous-avatar-non-binary-queer-person_23-2151100269.jpg?t=st=1720277317~exp=1720280917~hmac=131407d0cd2c4d6d0cd313ee799bb259e9d8255230e789861a0a31f571eb9f8a&w=826"
+                className={styles.userIcon}
+              ></img>
+              <div className={styles.profileInfo}>
+                <h1 className={styles.userName}>User1234</h1>
+                <p className={styles.status}>Online</p>
+              </div>
+            </div>
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
