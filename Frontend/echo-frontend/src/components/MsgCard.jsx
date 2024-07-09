aa14f259e0e898d689802102b923b85928e9aebf
mervynleong <changpeng66@gmail.com>
2024-07-09 21:45:43 +0800

    fixed an admin role into delete msg

--- Frontend/echo-frontend/src/components/MsgCard.jsx
+++ Frontend/echo-frontend/src/components/MsgCard.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./css/MsgCard.module.css";
 import { useQuery } from "@tanstack/react-query";
+import UserContext from "../context/user";
 
 const MsgCard = (props) => {
+  const userCtx = useContext(UserContext);
   const formatTime = (date) => {
     const parsedDate = new Date(date);
     return parsedDate.toLocaleString("en-GB", {
@@ -32,12 +34,21 @@ const MsgCard = (props) => {
 
   return (
     <div className={styles.cardContainer}>
-      <div
-        className={styles.delBtn}
-        onClick={() => props.handleDelete(props.id)}
-      >
-        <i className="fa-solid fa-xmark"></i>
-      </div>
+      {userCtx.role === "admin" ? (
+        <div
+          className={styles.delBtn}
+          onClick={() => props.handleDelete(props.id)}
+        >
+          <i className="fa-solid fa-xmark"></i>
+        </div>
+      ) : (
+        <div
+          className={styles.delBtn}
+          onClick={() => console.log("You are not allowed to delete message")}
+        >
+          <i className="fa-solid fa-xmark"></i>
+        </div>
+      )}
       <div className={styles.updateBtn}>
         <i className="fa-regular fa-pen-to-square"></i>
       </div>
