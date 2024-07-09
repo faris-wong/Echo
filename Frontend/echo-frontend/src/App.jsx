3fc6e5c2195a039f1af5ef5ad3f176197d0b0948
ashferoz <ash.safrina@gmail.com>
2024-07-09 16:11:47 +0800

    remove getProfileByAuth prop

--- Frontend/echo-frontend/src/App.jsx
+++ Frontend/echo-frontend/src/App.jsx
@@ -56,11 +56,7 @@ const App = () => {
               <Route
                 path="Profile"
                 element={
-                  <ProfilePage
-                    communityId={communityID}
-                    authID={authID}
-                    // getProfileByAuth={getProfileByAuth}
-                  />
+                  <ProfilePage communityId={communityID} authID={authID} />
                 }
               />
               {!accessToken && showLogin && (
